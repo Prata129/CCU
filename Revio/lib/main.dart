bb295b5748f2322b8dc563efdd6131d103ba897d
miguel-dasilva <56372014+miguel-dasilva@users.noreply.github.com>
2022-01-20 18:22:29 +0000

    Added backend

--- Revio/lib/main.dart
+++ Revio/lib/main.dart
@@ -1,100 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:revio/ui/homepage.dart';
+import 'package:revio/ui/auth/loginpage.dart';
+import 'package:revio/service/auth/authentication_service.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance)
+          ),
+        
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges,
+          initialData: null),
+      ],
+      child: MaterialApp(
+        title: 'RevioDemo',
+        home: AuthenticationWrapper(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-   @override
+  @override 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF222222),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container( 
-                  width: MediaQuery.of(context).size.width,
-                  height:250,
-                  child: Image.asset('assets/images/Capturar.PNG')),
-              ),
-            ),
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFF737373),
-                    focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white)),
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.white),
-                    hintText: 'Enter valid email id as abc@gmail.com'),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                  left: 30.0, right: 30.0, top: 30, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                style: TextStyle(color: Colors.white),
-                obscureText: true,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFF737373),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.white),
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 40),
-              height: 57,
-              width: 152,
-              decoration: BoxDecoration(
-                  color: const Color(0xFFE5BE58), borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
-                onPressed: () {
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.black, fontSize: 25),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 130,
-            ),
-            const Text('New User? Create Account')
-          ],
-        ),
-      ),
-    );
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return LoginPage();
   }
 }
---
+++ Revio/lib/ui/auth/loginpage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:revio/service/auth/authentication_service.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF222222),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container( 
+                  width: MediaQuery.of(context).size.width,
+                  height:250,
+                  child: Image.asset('assets/images/Capturar.PNG')),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: emailController,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFF737373),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 30.0, right: 30.0, top: 30, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                controller: passwordController,
+                style: const TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFF737373),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 40),
+              height: 57,
+              width: 152,
+              decoration: BoxDecoration(
+                  color: const Color(0xFFE5BE58), borderRadius: BorderRadius.circular(10)),
+              child: TextButton(
+                onPressed: () {
+                  context.read<AuthenticationService>().signIn(
+                    email: emailController.text, 
+                    password: passwordController.text.trim()
+                    );
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            const Text('New User? Create Account')
+          ],
+        ),
+      ),
+    );
+  }
+}
